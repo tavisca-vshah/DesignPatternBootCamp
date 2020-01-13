9a3266ac1d3039476990cb909b114435d6818e94
Vikram shah <vshah@tavisca.com>
2020-01-13 10:10:44 +0530

    first commit

---
+++ DesignBootcamp/ConcreteCollection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignBootcamp
+{
+    class ConcreteCollection
+    {
+        private List<Employee> listEmployees = new List<Employee>();
+        //Create Iterator
+       
+        // Gets item count
+        public int Count
+        {
+            get { return listEmployees.Count; }
+        }
+        //Add items to the collection
+        public void AddEmployee(Employee employee)
+        {
+            listEmployees.Add(employee);
+        }
+        //Get item from collection
+        public Employee GetEmployee(int IndexPosition)
+        {
+            return listEmployees[IndexPosition];
+        }
+    }
+
+}
